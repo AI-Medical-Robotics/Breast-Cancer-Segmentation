dbda2ff25076ce2b6f2bda68ecdec63619ea3000
james94 <jamesmedel94@gmail.com>
2022-11-13 20:29:01 -0800

    Added Rasa code to run lesion clf & seg: Just need each teammates models for testing

--- diagnosis_va/actions/actions.py
+++ diagnosis_va/actions/actions.py
@@ -4,24 +4,46 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
+import os
+import cv2
+import time
+import random
+import numpy as np
+import pandas as pd
 
 # from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk import Action #, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+
+import matplotlib.pyplot as plt
+
+from PIL import Image
+
+from tensorflow.keras.models import load_model
+import h5py
+from tensorflow.keras import __version__ as keras_version
+
+import xml.etree.ElementTree as ET
+from sklearn.preprocessing import LabelEncoder
+
+class ActionRunLesionClassification(Action):
+    def name(self):
+        return "action_run_lesion_classification"
+    
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(text="Running Breast Cancer Classifier")
+
+class ActionRunLesionSegmentation()
+    def name(self):
+        return "action_run_lesion_segmentation"
+    
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(text="Running Breast Cancer Segmentation")
+
+class ActionRunLesionVolRendering()
+    def name(self):
+        return "action_run_lesion_vol_rendering"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(text="Running Breast Cancer Volume Rendering")
