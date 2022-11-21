dc5909e7193c39d047f899d167910109111237f9
james94 <jamesmedel94@gmail.com>
2022-11-21 04:13:26 -0800

    Added comments to main.py

--- src/main.py
+++ src/main.py
@@ -1,36 +1,8 @@
 # Supports preparing, building, training, evaluating and deploying
 # breast cancer segmentation and classification models in the DL pipeline
 # common
-import os
-import keras
-import numpy as np
-import pandas as pd
-from glob import glob
-import tensorflow as tf
 
-# Data
-#from keras.preprocessing.image import load_img, img_to_array
-from keras.utils import load_img
-from keras.utils import img_to_array
-from keras.utils import to_categorical
-
-# Data Viz
-import matplotlib.pyplot as plt
-
-# Model 
-
-from keras import layers
-from keras import models
-
-# Callbacks 
-from keras.callbacks import Callback
-from keras.callbacks import EarlyStopping
-from keras.callbacks import ModelCheckpoint
-from tf_explain.callbacks.grad_cam import GradCAM
-#https://tf-explain.readthedocs.io/en/latest/usage.html
-
-# Metrics
-from keras.metrics import MeanIoU
-
+# TODO: figure out if a main.py script will be used since we're planning to transition
+# our code to rasa
 if __name__ == "__main__":
-    
+    pass
