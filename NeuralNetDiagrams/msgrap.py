83d06f501fd503cf21cda24d9c0e0080062a150c
james94 <jamesmedel94@gmail.com>
2022-11-23 14:32:46 -0800

    Minor update to msgrap.py

--- NeuralNetDiagrams/msgrap.py
+++ NeuralNetDiagrams/msgrap.py
@@ -7,19 +7,6 @@ from pycore.blocks  import *
 # Author: James Guzman (Medel)
 # MSGRAP Network Architecture
 
-def block_Unconv( name, botton, top, s_filer=256, n_filer=64, offset="(1,0,0)", size=(32,32,3.5), opacity=0.5 ):
-    return [
-        to_UnPool(  name='unpool_{}'.format(name),    offset=offset,    to="({}-east)".format(botton),         width=1,              height=size[0],       depth=size[1], opacity=opacity ),
-        to_ConvRes( name='ccr_res_{}'.format(name),   offset="(0,0,0)", to="(unpool_{}-east)".format(name),    s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1], opacity=opacity ),       
-        to_Conv(    name='ccr_{}'.format(name),       offset="(0,0,0)", to="(ccr_res_{}-east)".format(name),   s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1] ),
-        to_ConvRes( name='ccr_res_c_{}'.format(name), offset="(0,0,0)", to="(ccr_{}-east)".format(name),       s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1], opacity=opacity ),       
-        to_Conv(    name='{}'.format(top),            offset="(0,0,0)", to="(ccr_res_c_{}-east)".format(name), s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1] ),
-        to_connection( 
-            "{}".format( botton ), 
-            "unpool_{}".format( name ) 
-            )
-    ]
-
 def block_Trans3Conv( name, botton, top, s_filer="", n_filer=64, offset="(1,0,0)", size=(32,32,3.5), opacity=0.5, caption="" ):
     return [
         # like Tranpose Conv (Upsampling)
